b5607a8247d7629c281fe0435b01be01426fc4d7
Neil Manvar <neil@sentry.io>
2020-11-10 12:56:22 -0800

    increase duration

--- flask/app.py
+++ flask/app.py
@@ -77,7 +77,7 @@ def process_order(cart):
 
     # lognormal(...) returns 1-10 + sleep for 3 seconds every two hours (first two hours)
     time_to_sleep = numpy.random.lognormal(0.75, .6, 1)[0] if datetime.now(timezone('America/Los_Angeles')).hour >= 14 else numpy.random.lognormal(1.5, .5, 1)[0]
-    time.sleep(time_to_sleep)
+    time.sleep(time_to_sleep + .5)
 
     for item in cart:
         if Inventory[item['type']] <= 0:
