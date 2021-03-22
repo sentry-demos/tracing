f154899f9b6c014759af1f8558121237ffa7654b
William Capozzoli <thinkocapo@gmail.com>
2021-03-22 18:06:56 -0500

    clean

--- flask/app.py
+++ flask/app.py
@@ -4,7 +4,6 @@ from flask_cors import CORS
 from dotenv import load_dotenv
 from db import get_all_tools, get_inventory, update_inventory
 from dotenv import load_dotenv
-# from datetime import datetime
 import datetime
 from pytz import timezone
 from utils import wait
@@ -25,13 +24,6 @@ from sentry_sdk.integrations.flask import FlaskIntegration
 RELEASE = None
 if os.environ.get("RELEASE") is None:
     print("Prod release needs to be None, and it was None")
-    # d = datetime.datetime.now()
-    # month = d.strftime("%m")
-    # if len(month) == 2:
-        # month = month[1:]
-    # day = d.strftime("%d")
-    # RELEASE = month + "." + day
-
     def week_number_of_month(date_value):
         return (date_value.isocalendar()[1] - date_value.replace(day=1).isocalendar()[1] + 1)
     date_given = datetime.datetime.today().date()
