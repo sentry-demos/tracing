5b85b94f82a1643bc921b00b3df68e919ac9ca24
William Capozzoli <thinkocapo@gmail.com>
2021-03-25 20:30:33 -0500

    templated

--- admin.sh
+++ admin.sh
@@ -21,3 +21,25 @@ COMMIT_SHA=`git rev-parse HEAD`
 WHOAMI=`whoami`
 ##gcloud run deploy $WHOAMI
 echo $WHOAMI
+
+
+# build_react
+cd react && source $(HOME)/.nvm/nvm.sh && nvm use && npm install && npm run build
+
+# setup_release
+
+# create_release
+
+# associate_commits
+
+# upload_sourcemaps
+
+
+#build
+#gcloud builds submit --substitutions=COMMIT_SHA=$(COMMIT_SHA) --config=cloudbuild.yaml
+
+# run flask
+gcloud run deploy admin-flask --image $(REPOSITORY)/workspace_flask:$(COMMIT_SHA) --platform managed --add-cloudsql-instances sales-engineering-sf:us-central1:tracing-db-pg --update-env-vars INSTANCE_CONNECTION_NAME="sales-engineering-sf:us-central1:tracing-db-pg",RELEASE=$(RELEASE)
+
+# run react
+# gcloud run deploy admin-react --image $(REPOSITORY)/workspace_react:$(COMMIT_SHA) --platform managed
