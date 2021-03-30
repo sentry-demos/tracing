893fc80b221e072d9d6d9d2d0d5f015e22f29e5e
William Capozzoli <thinkocapo@gmail.com>
2021-03-30 09:47:19 -0500

    line breaks

--- admin.sh
+++ admin.sh
@@ -18,26 +18,30 @@ PREFIX=static/js
 echo $RELEASE
 REPOSITORY=us.gcr.io/sales-engineering-sf
 COMMIT_SHA=`git rev-parse HEAD`
+
 ##gcloud run deploy admin
 WHOAMI=`whoami`
 ##gcloud run deploy $WHOAMI
 echo $WHOAMI
+
 # build_react
-#rm -rf build
 cd react && rm -rf build && npm install && npm run build
+
 # create_release
 sentry-cli releases -o $SENTRY_ORG new -p $SENTRY_PROJECT $RELEASE
-echo 'done'
+
 # associate_commits
 sentry-cli releases -o $SENTRY_ORG -p $SENTRY_PROJECT set-commits --auto $RELEASE
-echo 'done'
+
 # upload_sourcemaps
 sentry-cli releases -o $SENTRY_ORG -p $SENTRY_PROJECT files $RELEASE upload-sourcemaps --url-prefix "~/static/js" --validate react/build/$PREFIX
-echo 'done'
-#build
+
+
+# build both images
 gcloud builds submit --substitutions=COMMIT_SHA=$COMMIT_SHA --config=cloudbuild.yaml
+
 # run flask
-gcloud run deploy admin-flask --image $REPOSITORY/workspace_flask:$COMMIT_SHA --platform managed --add-cloudsql-instances sales-engineering-sf:
-us-central1:tracing-db-pg --update-env-vars INSTANCE_CONNECTION_NAME="sales-engineering-sf:us-central1:tracing-db-pg"
+gcloud run deploy admin-flask --image $REPOSITORY/workspace_flask:$COMMIT_SHA --platform managed --add-cloudsql-instances sales-engineering-sf:us-central1:tracing-db-pg --update-env-vars INSTANCE_CONNECTION_NAME="sales-engineering-sf:us-central1:tracing-db-pg"
+
 # run react
 gcloud run deploy admin-react --image $REPOSITORY/workspace_react:$COMMIT_SHA --platform managed
