3a9c1bb630806391470a0cae2d92a6f239beaaee
William Capozzoli <thinkocapo@gmail.com>
2021-03-30 09:38:46 -0500

    release week5

--- admin.sh
+++ admin.sh
@@ -10,41 +10,34 @@ elif [ "$day" -ge 15 ] &&  [ "$day" -le 21 ]; then
   week=3
 elif [ "$day" -ge 22 ]; then
   week=4
+elif [ "day" -ge 29]; then
+  week=5
 fi
 RELEASE="$month.$week"
 PREFIX=static/js
 echo $RELEASE
 REPOSITORY=us.gcr.io/sales-engineering-sf
 COMMIT_SHA=`git rev-parse HEAD`
-
 ##gcloud run deploy admin
 WHOAMI=`whoami`
 ##gcloud run deploy $WHOAMI
 echo $WHOAMI
-
-
 # build_react
-#cd react && source wcap/.nvm/nvm.sh && nvm use && npm install && npm run build
-# cd react && npm install && npm run build
-
+#rm -rf build
+cd react && rm -rf build && npm install && npm run build
 # create_release
 sentry-cli releases -o $SENTRY_ORG new -p $SENTRY_PROJECT $RELEASE
 echo 'done'
 # associate_commits
 sentry-cli releases -o $SENTRY_ORG -p $SENTRY_PROJECT set-commits --auto $RELEASE
 echo 'done'
-
 # upload_sourcemaps
 sentry-cli releases -o $SENTRY_ORG -p $SENTRY_PROJECT files $RELEASE upload-sourcemaps --url-prefix "~/static/js" --validate react/build/$PREFIX
 echo 'done'
-
-echo $COMMIT_SHA
-
 #build
 gcloud builds submit --substitutions=COMMIT_SHA=$COMMIT_SHA --config=cloudbuild.yaml
-
 # run flask
-gcloud run deploy admin-flask --image $REPOSITORY/workspace_flask:$COMMIT_SHA --platform managed --add-cloudsql-instances sales-engineering-sf:us-central1:tracing-db-pg --update-env-vars INSTANCE_CONNECTION_NAME="sales-engineering-sf:us-central1:tracing-db-pg",RELEASE=$RELEASE
-
+gcloud run deploy admin-flask --image $REPOSITORY/workspace_flask:$COMMIT_SHA --platform managed --add-cloudsql-instances sales-engineering-sf:
+us-central1:tracing-db-pg --update-env-vars INSTANCE_CONNECTION_NAME="sales-engineering-sf:us-central1:tracing-db-pg"
 # run react
 gcloud run deploy admin-react --image $REPOSITORY/workspace_react:$COMMIT_SHA --platform managed
