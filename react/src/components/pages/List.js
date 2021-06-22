2f2485b87818313a8d36b9832228af4b76cf25cc
sentryadam0000345 <65051899+sentryadam0000345@users.noreply.github.com>
2021-06-21 23:34:54 -0700

    Update List.js

--- react/src/components/pages/List.js
+++ react/src/components/pages/List.js
@@ -1,22 +1,41 @@
 import React, { Component } from "react";
+import { connect } from 'react-redux';
+import * as Sentry from '@sentry/react';
+import { Button } from 'react-bootstrap'
+
+
+function notAFunctionError() {
+  var someArray = [{ func: function () {}}];
+  someArray[1].func();
+}
+
+function uriError() {
+  decodeURIComponent('%');
+}
+
+function syntaxError() {
+  eval('foo bar');
+}
+
+function rangeError() {
+  throw new RangeError('Parameter must be between 1 and 100');
+}
+
 
 class List extends Component {
     render() {
       return (
         <div>
-          <h2>STUFF</h2>
-          <p>Mauris sem velit, vehicula eget sodales vitae,
-          rhoncus eget sapien:</p>
-          <ol>
-            <li>Nulla pulvinar diam</li>
-            <li>Facilisis bibendum</li>
-            <li>Vestibulum vulputate</li>
-            <li>Eget erat</li>
-            <li>Id porttitor</li>
-          </ol>
+          <h2>Error Buttons</h2>
+          <Button variant="primary" onClick={notAFunctionError}>Not A Function</Button>
+          <Button variant="primary" onClick={uriError}>URI Error</Button>
+          <Button variant="primary" onClick={syntaxError}>Syntax Error</Button>
+          <Button variant="primary" onClick={rangeError}>Range Error</Button>
         </div>
       );
     }
   }
    
-  export default List;
+  export default connect(
+    null,
+  )(Sentry.withProfiler(List, { name: "List"}))
